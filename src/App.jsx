d2f0fcbdb9b345da019c923883a8182e859e2c42
Francisco E Gutierrez <142462584+frangu617@users.noreply.github.com>
2024-05-16 08:57:15 -0700

    working on home and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Container } from "@mui/material";
-import './App.css'
-import NavBar from './components/NavBar'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import AboutMe from "./components/AboutMe";
+import Portfolio from "./components/Portfolio";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      return (
-          <CssBaseline />
-          <Router>
-            <AppBar position="static" style={{ marginBottom: "20px" }}>
-              <Toolbar>
-                <Typography
-                  variant="h6"
-                  style={{ flexGrow: 1, marginLeft: "20px" }}
-                ></Typography>
-                <NavBar />
-              </Toolbar>
-            </AppBar>
-            <Container>
-              <Routes>
-                
-                <Route path="/" element={<Home />} />                
-                
-              </Routes>
-            </Container>
-          </Router>
-      )
+      <Router>
+        <AppBar position="static" style={{ marginBottom: "20px" }}>
+          <Toolbar>
+            <Typography
+              variant="h6"
+              style={{ flexGrow: 1, marginLeft: "20px" }}
+            ></Typography>
+            <NavBar />
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/aboutMe" element={<AboutMe />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+          </Routes>
+        </Container>
+      </Router>
     </>
   );
 }
 
-export default App
+export default App;
